4291021b468b4d77db5f42a34abf68155de08fc0
hoanghohohaha <hoangng@cs.washington.edu>
2022-06-08 15:43:26 -0700

    wrro

--- solution.py
+++ solution.py
@@ -20,7 +20,8 @@ def parse_message(data, start, bit_len):
     bits_str = ""
     for p in range(8):
         pos = start_data + bit_len*p
-        p1, p2 = A[int(pos): int(pos + bit_len/2)], A[int(pos + bit_len/2): int(pos + bit_len)]
+        p1, p2 = A[int(pos): int(pos + bit_len/2)
+                   ], A[int(pos + bit_len/2): int(pos + bit_len)]
         avg1, avg2 = np.average(p1), np.average(p2)
         if avg1 < avg2:
             bits_str += "0"
@@ -33,7 +34,8 @@ def parse_message(data, start, bit_len):
     bits_str = ""
     for p in range(8, 32):
         pos = start_data + bit_len * p
-        p1, p2 = A[int(pos): int(pos + bit_len/2)], A[int(pos + bit_len/2): int(pos + bit_len)]
+        p1, p2 = A[int(pos): int(pos + bit_len/2)
+                   ], A[int(pos + bit_len/2): int(pos + bit_len)]
         avg1, avg2 = np.average(p1), np.average(p2)
         if avg1 < avg2:
             bits_str += "0"
@@ -53,7 +55,8 @@ def parse_message(data, start, bit_len):
         bits_str = ""
         for p in range(32, 88):
             pos = start_data + bit_len*p
-            p1, p2 = A[int(pos): int(pos + bit_len/2)], A[int(pos + bit_len/2): int(pos + bit_len)]
+            p1, p2 = A[int(pos): int(pos + bit_len/2)
+                       ], A[int(pos + bit_len/2): int(pos + bit_len)]
             avg1, avg2 = np.average(p1), np.average(p2)
             if avg1 < avg2:
                 bits_str += "0"
@@ -67,16 +70,17 @@ def parse_message(data, start, bit_len):
         # Type Code:
         tc, ec = int(bits_str[:5], 2), int(bits_str[5:8], 2)
         # print("DF:", df, "TC:", tc)
-        
+
         # 1 - 4  Aircraft identification
         # 5 - 8  Surface position
         # 9 - 18  Airborne position (w/ Baro Altitude)
         # 19  Airborne velocities
 
-        if tc >= 1 and tc <= 4: # and (df == 17 or df == 18):
+        if tc >= 1 and tc <= 4:  # and (df == 17 or df == 18):
             print("Aircraft address:", address)
             print("Data:")
-            print(bits_str[:8], bits_str[8:14],  bits_str[14:20], bits_str[20:26], bits_str[26:32], bits_str[32:38], bits_str[38:44])
+            print(bits_str[:8], bits_str[8:14],  bits_str[14:20],
+                  bits_str[20:26], bits_str[26:32], bits_str[32:38], bits_str[38:44])
 
             symbols = "#ABCDEFGHIJKLMNOPQRSTUVWXYZ#####_###############0123456789######"
             code_str = ""
@@ -88,7 +92,8 @@ def parse_message(data, start, bit_len):
         if tc == 11:
             print("Aircraft address:", address)
             print("Data: (11)")
-            print(bits_str[:8], bits_str[8:20],  bits_str[20:22], bits_str[22:22+17], bits_str[39:39+17])
+            print(bits_str[:8], bits_str[8:20],  bits_str[20:22],
+                  bits_str[22:22+17], bits_str[39:39+17])
 
             # Bit 22 contains the F flag which indicates which CPR format is used (odd or even)
             # First frame has F flag = 0 so is even and the second frame has F flag = 1 so odd
@@ -113,25 +118,26 @@ def parse_message(data, start, bit_len):
             print("Aircraft address:", address)
             print("Data:")
             # print(bits_str)
-            print(bits_str[:5], bits_str[5:8], bits_str[8:10], bits_str[10:13], bits_str[13] ,bits_str[14:24], bits_str[24], bits_str[25:35], bits_str[35:36], bits_str[36:65])
+            print(bits_str[:5], bits_str[5:8], bits_str[8:10], bits_str[10:13], bits_str[13],
+                  bits_str[14:24], bits_str[24], bits_str[25:35], bits_str[35:36], bits_str[36:65])
 
             subtype = int(bits_str[5:8], 2)
             # https://mode-s.org/decode/adsb/airborne-velocity.html
             spd, hdg, rocd = -1, -1, -1
             if subtype == 1 or subtype == 2:
                 print("Velocity Subtype 1: Ground speed")
-            
+
                 v_ew_sign = int(bits_str[13], 2)
                 v_ew = int(bits_str[14:24], 2) - 1       # east-west velocity
-                
+
                 v_ns_sign = int(bits_str[24], 2)
                 v_ns = int(bits_str[25:35], 2) - 1       # north-south velocity
-                
+
                 v_we = -1*v_ew if v_ew_sign else v_ew
                 v_sn = -1*v_ns if v_ns_sign else v_ns
-                
+
                 spd = math.sqrt(v_sn*v_sn + v_we*v_we)  # unit in kts
-                
+
                 hdg = math.atan2(v_we, v_sn)
                 hdg = math.degrees(hdg)                 # convert to degrees
                 hdg = hdg if hdg >= 0 else hdg + 360    # no negative val
@@ -139,7 +145,7 @@ def parse_message(data, start, bit_len):
                 print("Subtype Subtype 3: Airspeed")
                 hdg = int(bits_str[14:24], 2)/1024.0*360.0
                 spd = int(bits_str[25:35], 2)
-            
+
             vr_sign = int(bits_str[36], 2)
             vr = int(bits_str[36:45], 2)
             rocd = -1*vr if vr_sign else vr         # rate of climb/descend
@@ -147,6 +153,7 @@ def parse_message(data, start, bit_len):
             print()
 
         # print()
+
 
 def calc_coordinates():
     def _cprN(lat, is_odd):
@@ -163,15 +170,15 @@ def calc_coordinates():
         except:
             # happens when latitude is +/-90 degree
             return 1
-    
+
     def floor_(x):
         return int(math.floor(x))
-  
+
     lat1b, lon1b, alt1b = "10111000111010011", "10000110111111000", "000101111001"
     lat2b, lon2b, alt2b = "10010011101011100", "10000011000011011", "000101110111"
     lat1, lon1, alt1 = int(lat1b, 2), int(lon1b, 2), int(alt1b, 2)
     lat2, lon2, alt2 = int(lat2b, 2), int(lon2b, 2), int(alt2b, 2)
-    
+
     # 131072 is 2^17, since CPR lat and lon are 17 bits each
     cprlat_even, cprlon_even = lat1/131072.0, lon1/131072.0
     cprlat_odd, cprlon_odd = lat2/131072.0, lon2/131072.0
@@ -193,7 +200,8 @@ def calc_coordinates():
 
     # Lon
     ni = _cprN(lat_even, 0)
-    m = floor_(cprlon_even * (_cprNL(lat_even)-1) - cprlon_odd * _cprNL(lat_even) + 0.5)
+    m = floor_(cprlon_even * (_cprNL(lat_even)-1) -
+               cprlon_odd * _cprNL(lat_even) + 0.5)
     lon = (360.0 / ni) * (m % ni + cprlon_even)
     print("Lat", lat_even, "Lon", lon)
 
@@ -207,16 +215,17 @@ def calc_coordinates():
         print("Alt (ft)", alt_ft)
 
 
-# fs, data = wavfile.read("adsb_20190311_191728Z_1090000kHz_RF.wav")
-raw = np.load("adsb.npy")
-data = np.array([np.real(raw), np.imag(raw)])
-fs = 1e6
+fs, data = wavfile.read("adsb_20190311_191728Z_1090000kHz_RF.wav")
+# raw = np.load("adsb.npy")
+# data = np.array([np.real(raw), np.imag(raw)])
+# fs = 1e6
 
 T = 1/fs
-
+print(data)
 print("Sample rate %f MS/s" % (fs / 1e6))
 print("Cnt samples %d" % len(data))
 print("Duration: %f s" % (T * len(data)))
+calc_coordinates()
 
 data = data.astype(float)
 
@@ -276,7 +285,6 @@ while pos < cnt - 16*1024:
             pos += 1
             break
         pos += 1
-
 
     sig_diff = start4 - start1
     if 20 < sig_diff < 25:
